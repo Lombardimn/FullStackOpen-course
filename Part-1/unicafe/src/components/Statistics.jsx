9558ea42d81b7c7ce75a38631f64a2763e3e7998
Matias Lombardi <matiiaslombardi@gmail.com>
2023-11-29 09:17:27 -0300

    1.7: unicafe, step 2

--- Part-1/unicafe/src/components/Statistics.jsx
+++ Part-1/unicafe/src/components/Statistics.jsx
@@ -1,10 +1,18 @@
 export const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = (good / total) * 100
+
   return (
     <div>
       <h2>Statistics</h2>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+
+      <p>Total: {total}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive}%</p>
     </div>
   )
 }

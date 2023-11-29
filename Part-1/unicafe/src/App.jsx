48f06807a3d4335e0b95d28ebdf9f8d985325d41
Matias Lombardi <matiiaslombardi@gmail.com>
2023-11-29 09:36:15 -0300

    1.10: unicafe, step 5

--- Part-1/unicafe/src/App.jsx
+++ Part-1/unicafe/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Statistics } from './components/Statistics'
+import { Button } from './components/Button'
 
 const App = () => {
   // save clicks of each button to its own state
@@ -11,9 +12,16 @@ const App = () => {
     <div>
       <h1>Unicafe</h1>
       <h2>give feedback</h2>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button 
+        handleClick={() => setGood(good + 1)} text="good" 
+      />
+      <Button
+        handleClick={() => setNeutral(neutral + 1)} text="neutral"
+      />
+      <Button
+        handleClick={() => setBad(bad + 1)} 
+        text="bad"
+      />
 
       {/* component to show the statistics */}
       <Statistics good={good} neutral={neutral} bad={bad} />
--- Part-1/unicafe/src/components/Statistics.jsx
+++ Part-1/unicafe/src/components/Statistics.jsx
@@ -1,3 +1,4 @@
+import { StatisticLine } from "./StatisticLine"
 export const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad
   const average = (good - bad) / total
@@ -10,13 +11,12 @@ export const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-
-      <p>Total: {total}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positive}%</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={`${positive} %`} />
     </div>
   )
 }

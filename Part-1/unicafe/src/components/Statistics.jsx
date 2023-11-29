ed1160c20a7737b83cbf85bf3b10d36a4821999f
Matias Lombardi <matiiaslombardi@gmail.com>
2023-11-29 09:25:47 -0300

    1.9: unicafe, step 4

--- Part-1/unicafe/src/components/Statistics.jsx
+++ Part-1/unicafe/src/components/Statistics.jsx
@@ -3,6 +3,10 @@ export const Statistics = ({ good, neutral, bad }) => {
   const average = (good - bad) / total
   const positive = (good / total) * 100
 
+  if(total === 0) {
+    return <p>No feedback given</p>
+  }
+
   return (
     <div>
       <h2>Statistics</h2>
